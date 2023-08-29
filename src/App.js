93b714c56e9657def2b722afd143efe9444b6dd1
James Florez <james_florez@insideranken.org>
2023-08-29 11:51:13 -0500

    push

--- src/App.js
+++ src/App.js
@@ -1,21 +1,18 @@
-import logo from './logo.svg';
 import './App.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faListSquares } from '@fortawesome/free-solid-svg-icons';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>Hello World</p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <button className='btn-light btn-lg'><FontAwesomeIcon icon={faListSquares} />Click</button>
       </header>
+      <main>
+      </main>
     </div>
   );
 }
