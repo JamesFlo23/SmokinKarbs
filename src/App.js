144fdfa712dbf4f248906c8f793cc1a6d286196e
James Florez <james_florez@insideranken.org>
2023-08-30 11:49:39 -0500

    push

--- src/App.js
+++ src/App.js
@@ -1,17 +1,28 @@
+import React from 'react';
 import './App.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faListSquares } from '@fortawesome/free-solid-svg-icons';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 
+
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>Hello World</p>
-      <button className='btn-light btn-lg'><FontAwesomeIcon icon={faListSquares} />Click</button>
+    <div id="App">
+      <header className="header" id='App-header'>
+        <h1>Smokin Karbs</h1>
+      <button className='btn-light btn-lg' id='navButton'><FontAwesomeIcon icon={faListSquares} />Navbar</button>
+        <ul id='navList' className='hidden'>
+          <li id='navItem1' className='navItem'>Home 'active'</li>
+          <li id='navItem2' className='navItem'>Inventory</li>
+            {/*Add types of products withing Inventory */}
+          <li id='navItem3' className='navItem'>Special Events and Offers</li>
+          <li id='navItem4' className='navItem'>Contact</li>
+            {/*Social media, copyright and direct contact info within */}
+        </ul>
       </header>
       <main>
+        <p>Hello</p>
       </main>
     </div>
   );
